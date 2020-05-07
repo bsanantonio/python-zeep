9d8922155b37235832b07e83e95d269e8d63e08f
Bryant San Antonio <bryant.sanantonio@lagunitas.com>
2020-05-06 17:52:30 -0700

    remove more checks

--- src/zeep/xsd/types/complex.py
+++ src/zeep/xsd/types/complex.py
@@ -200,10 +200,7 @@ class ComplexType(AnyType):
 
             # Check if all children are consumed (parsed)
             if elements:
-                if schema.settings.strict:
-                    raise XMLParseError("Unexpected element %r" % elements[0].tag)
-                else:
-                    init_kwargs["_raw_elements"] = elements
+                init_kwargs["_raw_elements"] = elements
 
         # Parse attributes
         if attributes:
